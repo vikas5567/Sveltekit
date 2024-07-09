68e18be2cafc8f8cc1ea31f89e8f1d9c9fba1b6d
vikas5567 <161954517+vikas5567@users.noreply.github.com>
2024-07-09 13:39:34 +0530

    Revert "init commit"
    This reverts commit 795f252f5939e14a4ab1cd499a035bc0268d25aa.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
-    import Navbar from '$lib/Navbar.svelte'
-    import Footer from '$lib/Footer.svelte'
-    import "../app.css"
+import Footer from '$lib/Footer.svelte'
+import Navbar from '$lib/Navbar.svelte'
+import "../app.css"
 </script>
-    
-    <Navbar />
-    <slot />
-    <Footer />
-    
+
+<Navbar />
+<slot />
+<Footer />
