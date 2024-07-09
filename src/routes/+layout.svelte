044a3418dd0127fa72e160da0687dd5c72128ed9
vikas5567 <161954517+vikas5567@users.noreply.github.com>
2024-07-09 13:32:17 +0530

    Reapply "init commit"
    This reverts commit c554ee8c1d8bae6fb3aa0eeb8271f44ae038d6cb.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
-import Footer from '$lib/Footer.svelte'
-import Navbar from '$lib/Navbar.svelte'
-import "../app.css"
+    import Navbar from '$lib/Navbar.svelte'
+    import Footer from '$lib/Footer.svelte'
+    import "../app.css"
 </script>
-
-<Navbar />
-<slot />
-<Footer />
+    
+    <Navbar />
+    <slot />
+    <Footer />
+    
--- src/routes/blog/[slug]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { sql } from "@vercel/postgres";
-
-export async function load({ locals }) {
-  return {
-    content: await sql`SELECT * from NAMES where user_id='${locals.user}'`
-  }
-}
-
-
